362e084dc70e4e340efbb2b097bbb0aeb40bd94c
Vijay Anand <vijayanand@Vijays-MacBook-Air.local>
2023-07-27 16:03:51 +0530

    first commit

---
+++ FirstProject/src/Content.js
@@ -0,0 +1,41 @@
+
+import React from 'react'
+import { FaTrashAlt } from'react-icons/fa'
+
+
+const Content = ({items , handleCheck, handleDelete }) => {
+
+  return (
+    <>
+      {(items.length) ? (
+        <ul>
+        {items.map((item) => (
+          <li className='item' key = {item.id}>
+            <input
+            type='checkbox'
+            onChange = {() => handleCheck(item.id)}
+            checked = {item.checked}
+            />
+            <label
+            style={(item.checked ) ? {textDecoration: 'line-through'} :null}
+            onDoubleClick={() => handleCheck(item.id)}
+            >{item.item}</label>
+            <FaTrashAlt 
+            role='button'
+            onClick={() => handleDelete(item.id)}
+            tabIndex='0'
+            aria-label={`Delete ${item.item}`}
+            />
+          </li>
+        ))}
+      </ul>
+      ) : (
+        <p>No items to display</p>
+      )
+    }    
+    </>
+    
+  )
+}
+
+export default Content
